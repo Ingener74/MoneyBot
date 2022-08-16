72b9e640f06887d7fce9dd60eeb19e458c46462b
Shnaider Pavel <shnaiderpasha@gmail.com>
2022-08-16 23:25:58 +0500

    Поправил файл проверок

--- check.sh
+++ check.sh
@@ -1,4 +1,4 @@
 PYTHONPATH=. pytest tests
-black com_gui money Purchase tests *.py --exclude com_gui/res --line-length 120
+black com_gui money tests *.py --exclude com_gui/res --line-length 120
 flake8
-mypy com_gui money Purchase tests *.py
+mypy com_gui money tests *.py
